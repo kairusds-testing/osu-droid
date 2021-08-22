31a9c947a40dafa2cf1a4d804dee770c8743ca86
kairusds <harveyhansjavier@gmail.com>
2021-08-22 23:08:50 +0800

    Debug initAccessibilityDetector XY

--- src/com/edlplan/ui/fragment/CheatedDialogFragment.java
+++ src/com/edlplan/ui/fragment/CheatedDialogFragment.java
@@ -18,7 +18,7 @@ public class CheatedDialogFragment extends ConfirmDialogFragment {
                     wakeLock.release();
                 }
             }
-        });
+        };
         super.show();
     }
 
