13cf03bfef7529520b21fe74e050c52dc9162503
kairusds <harveyhansjavier@gmail.com>
2021-08-28 22:23:30 +0800

    PermissionActivity: add missing import Settings

--- src/ru/nsu/ccfit/zuev/osu/PermissionActivity.java
+++ src/ru/nsu/ccfit/zuev/osu/PermissionActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.net.Uri;
 import android.util.Log;
 
