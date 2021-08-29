7ca584b4bab0e186fb0b2cc8605b88da6552c04f
kairusds <harveyhansjavier@gmail.com>
2021-08-30 02:45:21 +0800

    initAccessibilityDetector: only detect AccessibilityServiceInfo.CAPABILITY_CAN_PERFORM_GESTURES, frgdialog_confirm: revert

--- src/ru/nsu/ccfit/zuev/osu/MainActivity.java
+++ src/ru/nsu/ccfit/zuev/osu/MainActivity.java
@@ -815,11 +815,11 @@ public class MainActivity extends BaseGameActivity implements
 
                 for(int i = 0; i < activeServices.size(); i++) {
                      int capabilities = activeServices.get(i).getCapabilities();
-                    if(/*(AccessibilityServiceInfo.CAPABILITY_CAN_RETRIEVE_WINDOW_CONTENT & capabilities) == AccessibilityServiceInfo.CAPABILITY_CAN_RETRIEVE_WINDOW_CONTENT
-                        || */(AccessibilityServiceInfo.CAPABILITY_CAN_PERFORM_GESTURES & capabilities) == AccessibilityServiceInfo.CAPABILITY_CAN_PERFORM_GESTURES) {
+                    if((AccessibilityServiceInfo.CAPABILITY_CAN_PERFORM_GESTURES & capabilities)
+                            == AccessibilityServiceInfo.CAPABILITY_CAN_PERFORM_GESTURES) {
                         if(!dialogShown && activityVisible) {
                             new ConfirmDialogFragment()
-                                .setMessage(R.string.message_suspicious_accessibility_services)
+                                .setMessage(R.string.message_autoclicker_detected)
                                 .setOnDismissListener(fragment -> cheatedExit())
                                 .showForResult(isAccepted -> cheatedExit());
                             dialogShown = true;
