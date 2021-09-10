eafbb254438856a737f98c83fc043fa805100013
kairusds <harveyhansjavier@gmail.com>
2021-09-10 23:54:43 +0800

    WebViewActivity: add debugger

--- src/ru/nsu/ccfit/zuev/osu/online/WebViewActivity.java
+++ src/ru/nsu/ccfit/zuev/osu/online/WebViewActivity.java
@@ -10,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import org.anddev.andengine.util.Debug;
+
 import ru.nsu.ccfit.zuev.osu.GlobalManager;
 import ru.nsu.ccfit.zuev.osu.MainActivity;
 import ru.nsu.ccfit.zuev.osuplus.R;
