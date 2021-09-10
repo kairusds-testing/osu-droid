ef4277c9dbdf11c38be92f2561bf771a05cd39fe
kairusds <harveyhansjavier@gmail.com>
2021-09-11 04:50:14 +0800

    WebViewActivity: remove progress bar, test user agent

--- src/ru/nsu/ccfit/zuev/osu/online/WebViewActivity.java
+++ src/ru/nsu/ccfit/zuev/osu/online/WebViewActivity.java
@@ -2,15 +2,12 @@ package ru.nsu.ccfit.zuev.osu.online;
 
 import android.app.Activity;
 import android.content.Intent;
-// import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebChromeClient;
 import android.webkit.JavascriptInterface;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -40,26 +37,10 @@ public class WebViewActivity extends AppCompatActivity {
 
         mActivity = GlobalManager.getInstance().getMainActivity();
 
-        RelativeLayout webviewProgress = (RelativeLayout) findViewById(R.id.webview_progress);
-        webviewProgress.setVisibility(View.VISIBLE);
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.webview_progressbar);
-        progressBar.setVisibility(View.VISIBLE);
-        // progressBar.getIndeterminateDrawable().setColorFilter(mActivity.getResources().getColor(R.color.colorAccent), PorterDuff.Mode.SRC_IN);
-        progressBar.setProgress(1);
-
         webview = (WebView) findViewById(R.id.webview);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.getSettings().setUserAgentString("osudroid");
-        webview.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                progressBar.setProgress(newProgress);
-                if(newProgress == 100) {
-                    webviewProgress.setVisibility(View.GONE);
-                    progressBar.setVisibility(View.GONE);
-                }
-            }
-        });
+        webview.setWebChromeClient(new WebChromeClient());
 
         switch(getIntent().getStringExtra(EXTRA_URL)) {
             case LOGIN_URL:
@@ -69,7 +50,7 @@ public class WebViewActivity extends AppCompatActivity {
             case REGISTER_URL:
                 webview.addJavascriptInterface(new RegisterTypeInterface(),
                     JAVASCRIPT_INTERFACE_NAME);
-                webview.loadUrl(REGISTER_URL);
+                webview.loadUrl("https://www.whatismybrowser.com/detect/what-is-my-user-agent");
                 break;
 
             case PROFILE_URL:
