96a48de4a5eba7b9148ce8104bc54ebd203a5023
kairusds <harveyhansjavier@gmail.com>
2021-09-10 21:49:58 +0800

    Fix WebViewClientImpl backwards compatibility

--- src/ru/nsu/ccfit/zuev/osu/online/WebViewClientImpl.java
+++ src/ru/nsu/ccfit/zuev/osu/online/WebViewClientImpl.java
@@ -1,20 +1,10 @@
 package ru.nsu.ccfit.zuev.osu.online;
 
-import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.webkit.WebResourceRequest;
 
 public class WebViewClientImpl extends WebViewClient {
 
-    // >= api 24
-    @Override
-    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-        if(request.getUrl().toString().startsWith(OnlineManager.host)) return false;
-        return true;
-    }
-
-    // < api 24
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
         if(url.startsWith(OnlineManager.host)) return false;
