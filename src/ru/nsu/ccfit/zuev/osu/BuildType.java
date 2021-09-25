77851e59e121b70dc45e2bd4e230e30e8be87aef
kairusds <harveyhansjavier@gmail.com>
2021-09-25 12:00:29 +0800

    Debug crashlytics

--- src/ru/nsu/ccfit/zuev/osu/BuildType.java
+++ src/ru/nsu/ccfit/zuev/osu/BuildType.java
@@ -4,6 +4,7 @@ import ru.nsu.ccfit.zuev.osuplus.BuildConfig;
 
 public class BuildType {
     public static boolean hasOnlineAccess() {
-        return BuildConfig.BUILD_TYPE.matches("(release|pre_release)");
+        return false;
+        // return BuildConfig.BUILD_TYPE.matches("(release|pre_release)");
     }
 }
