8f161b18b13696f3a579b08bba3f2300597277d7
kairusds <harveyhansjavier@gmail.com>
2021-09-27 04:48:23 +0800

    Fix WebViewFragment url constants

--- src/com/edlplan/ui/fragment/WebViewFragment.java
+++ src/com/edlplan/ui/fragment/WebViewFragment.java
@@ -23,8 +23,8 @@ import ru.nsu.ccfit.zuev.osuplus.R;
 
 public class WebViewFragment extends BaseFragment {
 
-    public static final String REGISTER_URL = OnlineManager.hostname + "user/?action=register";
-    public static final String PROFILE_URL = OnlineManager.hostname + "profile.php?uid=";
+    public static final String REGISTER_URL = "https://" + OnlineManager.hostname + "user/?action=register";
+    public static final String PROFILE_URL = "https://" + OnlineManager.hostname + "profile.php?uid=";
 
     private WebView webview;
     private String url;
