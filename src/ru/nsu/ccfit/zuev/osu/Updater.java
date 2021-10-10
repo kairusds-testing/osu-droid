445bced2043bfc00c2a8fc5e6d153d000483d3f5
kairusds <harveyhansjavier@gmail.com>
2021-10-10 20:15:43 +0800

    Debug Updater

--- src/ru/nsu/ccfit/zuev/osu/Updater.java
+++ src/ru/nsu/ccfit/zuev/osu/Updater.java
@@ -11,7 +11,7 @@ import com.google.gson.Gson;
 
 import java.io.IOException;
 
-import okhttp3.Response;
+import okhttp3.ResponseBody;
 import okhttp3.Request;
 
 import org.anddev.andengine.util.Debug;
